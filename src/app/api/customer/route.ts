e70d6dac76d4c5d22de3e811e895cf36607ddd57
Seth Charles Palileo <sethcharles28@gmail.com>
2025-05-22 23:03:20 +0800

    added POST route for adding customer to db

---
+++ src/app/api/customer/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/store/store";
+import { CustomerInfoSchema } from "@/lib/schema";
+
+export async function POST(req: NextRequest) {
+  try {
+    const customerInfoData = await req.json();
+    const validation = CustomerInfoSchema.safeParse(customerInfoData);
+    if (!validation.success) {
+      return NextResponse.json(
+        { errors: validation.error.errors },
+        { status: 400 },
+      );
+    }
+    db.customers.insert(customerInfoData);
+    return NextResponse.json({ message: "Customer info saved" });
+  } catch (error) {
+    console.error("Error processing customer info:", error);
+    return NextResponse.json(
+      { error: "Failed to process customer info" },
+      { status: 500 },
+    );
+  }
+}
